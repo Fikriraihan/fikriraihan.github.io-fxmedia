79a2208aed4d7fa567dd97b3e3dd4ebe291f35be
Fikriraihan <fikri.raihan4@gmail.com>
2022-04-12 12:54:38 +0700

    add modified

--- variable.js
+++ variable.js
@@ -39,3 +39,16 @@ function showfield() {
 $(".tutup").mouseleave(function () {
   $(this).fadeOut();
 });
+
+function myFunction(x) {
+  if (x.matches) {
+    // If media query matches
+    $("#brandlogo").hide();
+  } else {
+    $("#brandlogo").show();
+  }
+}
+
+var x = window.matchMedia("(max-width: 700px)");
+myFunction(x); // Call listener function at run time
+x.addListener(myFunction); // Attach listener function on state changes
